60290eb06aa87e84940216a8e2dfd3e26fffb3ca
Praveen Manchana <praveen.manchana@ganakasolutions.com.au>
2019-10-25 21:41:02 +1100

    Set up the Project for Catalog Extensions

---
+++ src/Plugin.Sample.CatalogExtension/Components/VariantPropertiesComponent.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections.Generic;
+using Sitecore.Commerce.Core;
+
+namespace Plugin.Sample.CatalogExtension.Components
+{
+    public class VariantPropertiesComponent : Component
+    {
+        public VariantPropertiesComponent()
+        {
+            Colors = new List<string>();
+            Sizes = new List<string>();
+            Styles = new List<string>();
+        }
+
+        public List<string> Colors { get; set; }
+        public List<string> Sizes { get; set; }
+        public List<string> Styles { get; set; }
+    }
+}
---
+++ src/Plugin.Sample.CatalogExtension/Pipelines/Blocks/GetSellableItemVariantPropertiesViewBlock.cs
@@ -0,0 +1,10 @@
+﻿using Sitecore.Commerce.Core;
+using Sitecore.Commerce.EntityViews;
+
+
+namespace Plugin.Sample.CatalogExtension.Pipelines.Blocks
+{
+    public class GetSellableItemVariantPropertiesViewBlock : PipelineBlock<EntityView, EntityView, CommercePipelineExecutionContext>
+    {
+    }
+}
